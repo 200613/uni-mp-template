77cf0f8f9ad96bff44d1df0a1368e2cab77555eb
王冉冉 <1945457312@qq.com>
2023-11-07 10:18:32 +0800

    安装windows

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,8 @@
 <template>
   <view class="index">index</view>
   <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="w-10 h-10 bg-pink-500"></div>
+  <div class="test bg-yellow-500 w-10 h-10"></div>
 </template>
 
 <style lang="scss">
---
+++ src/utils/http.ts
@@ -0,0 +1,57 @@
+// src/utils/http.ts
+import { useMemberStore } from '@/stores'
+// 请求基地址
+const baseURL = 'https://www.dreamcode.site/shop-api'
+// 拦截器配置
+const httpInterceptor = {
+  // 拦截前触发
+  invoke(options: UniApp.RequestOptions) {
+    // 1. 非 http 开头需拼接地址
+
+    if (!options.url.startsWith('http')) {
+      options.url = baseURL + options.url
+    }
+
+    // 2. 请求超时
+
+    options.timeout = 10000
+
+    // 3. 添加小程序端请求头标识
+
+    options.header = {
+      'source-client': 'miniapp',
+
+      ...options.header
+    }
+
+    // 4. 添加 token 请求头标识
+
+    const memberStore = useMemberStore()
+
+    const token = memberStore.profile?.token
+
+    if (token) {
+      options.header.Authorization = token
+    }
+  }
+}
+
+// 拦截 request 请求
+
+uni.addInterceptor('request', httpInterceptor)
+
+// 拦截 uploadFile 文件上传
+
+uni.addInterceptor('uploadFile', httpInterceptor)
+
+/**
+ * 请求函数
+ * @param  UniApp.RequestOptions
+ * @returns Promise
+ *  1. 返回 Promise 对象，用于处理返回值类型
+ *  2. 获取数据成功
+ *    2.1 提取核心数据 res.data
+ *    2.2 添加类型，支持泛型
+ *  3. 获取数据失败
+ *    3.1 401
+ * */
